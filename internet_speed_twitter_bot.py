abc67acf9fc4b674a6042df44970347c34156d56
Soulbolt <soulbolt@gmail.com>
2024-01-06 14:32:39 -0700

    Implemented new method to open twitter site, log in via google account, create new tweet with message providing speed results. Added quit method as well.

--- internet_speed_twitter_bot.py
+++ internet_speed_twitter_bot.py
@@ -11,7 +11,7 @@ TWITTER_PASSWORD = "password_here"
 OPTIONS = CHROME_OPTIONS.add_experimental_option("detach", True)
 
 class InternetSpeedTwitterBot:
-    
+
     def __init__(self):
         self.driver = webdriver.Chrome(options=OPTIONS)
         self.down = 0
@@ -27,3 +27,21 @@ class InternetSpeedTwitterBot:
         self.down = print(f"Download speed: {down_speed.text} Mbps")
         up_speed = self.driver.find_element(By.CLASS_NAME, value="upload-speed")
         self.up = print(f"Upload speed: {up_speed.text} Mbps")
+
+    def tweet_complaint(self):
+        self.driver.get("https://twitter.com")
+        time.sleep(3)
+        login = self.driver.find_element(By.LINK_TEXT, value="Sign in")
+        login.click()
+        time.sleep(3)
+        sign_in_with_google = self.driver.find_element(By.LINK_TEXT, value="Sign in with Google")
+        sign_in_with_google.click()
+        time.sleep(3)
+        tweet_button = self.driver.find_element(By.LINK_TEXT, value="Tweet")
+        tweet_button.click()
+        active = self.driver.switch_to.active_element
+        active.send_keys(f"Download speed was {self.down} Mbps and Upload was {self.up} Mbps. Fix it please")
+        time.sleep(15)
+
+    def quit(self):
+        self.driver.quit()
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from internet_speed_twitter_bot import InternetSpeedTwitterBot
 
 bot = InternetSpeedTwitterBot()
 bot.test_speed()
+bot.tweet_complaint()
+bot.quit()
